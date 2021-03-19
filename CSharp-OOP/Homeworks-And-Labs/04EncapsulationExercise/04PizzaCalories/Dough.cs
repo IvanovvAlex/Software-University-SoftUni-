dae239411b6390b2753c2743a1032202184f01b5
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-03-19 16:38:51 +0200

    Add

---
+++ CSharp-OOP/Homeworks-And-Labs/04EncapsulationExercise/04PizzaCalories/Dough.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04PizzaCalories
+{
+    public class Dough
+    {
+        private string flourType;
+        private int weight;
+
+        public string FlourType
+        {
+            get { return flourType; }
+            set { flourType = value; }
+        }       
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+
+    }
+}
