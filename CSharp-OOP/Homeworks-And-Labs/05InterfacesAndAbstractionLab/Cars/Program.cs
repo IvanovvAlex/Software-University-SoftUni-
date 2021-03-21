7f0cd49339f483e4a5a44dee6e3065a6de4a8d22
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-03-22 00:14:28 +0200

    Add

---
+++ CSharp-OOP/Homeworks-And-Labs/05InterfacesAndAbstractionLab/Cars/Program.cs
@@ -0,0 +1,22 @@
+﻿using Cars.Contracts;
+using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            seat.Start();
+            seat.Stop();
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+            tesla.Start();
+            tesla.Stop();
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+
+        }
+    }
+}
