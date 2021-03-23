15965c1f7c560ee7ebc49f0080c99ba29acf597a
IvanovvAlex <alexgeorgievivanov2003@gmail.com>
2021-03-23 10:35:07 +0200

    Add

--- CSharp-OOP/Homeworks-And-Labs/06InterfacesAndAbstractionExercise/07MilitaryElite/Program.cs
+++ CSharp-OOP/Homeworks-And-Labs/06InterfacesAndAbstractionExercise/07MilitaryElite/Program.cs
@@ -2,11 +2,11 @@
 
 namespace _07MilitaryElite
 {
-    class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
