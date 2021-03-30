04391eb653fddd9ce47ec6f4c602a4d04e78ec46
Alex Ivanov <alexivanovv4@gmail.com>
2021-03-30 09:30:03 +0300

    Add

---
+++ CSharp-OOP/Homeworks-And-Labs/07PolymorphismLab/Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;       
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public string FavouriteFood
+        {
+            get { return favouriteFood; }
+            private set { favouriteFood = value; }
+        }
+
+       public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood}";
+        }
+
+    }
+}
