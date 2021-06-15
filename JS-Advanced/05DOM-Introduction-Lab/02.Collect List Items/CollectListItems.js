40081881e416136e9e41bb2e8372a21e7916635c
Alex Ivanov <alexivanovv4@gmail.com>
2021-06-15 13:00:49 +0300

    Edit

--- JS-Advanced/05DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/05DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    let listRef = document.getElementById('items');
+    let textAreaRef = document.getElementById('result');
+    let listOfElements = listRef.querySelectorAll('li');
+
+    for (const element of listOfElements) {
+        textAreaRef.textContent += `${element.textContent} \n`
+    }
+textAreaRef.textContent = textAreaRef.textContent.trim();
 }
