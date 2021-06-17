98ce311053b80324c7c378d6b97db9f65b3d9644
Alex Ivanov <alexivanovv4@gmail.com>
2021-06-17 12:12:14 +0300

    Edit and Add

--- JS-Advanced/05DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/05DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    const trRef = document.querySelectorAll('tr');
+    let counter = 1;
+    for (const tr of trRef) {
+        if (counter % 2 == 0) {
+            tr.style.background = "Teal";
+        }
+        counter++;
+    }
 }
--- JS-Advanced/05DOM-Introduction-Lab/06.Sum Table/SumTable.js
+++ JS-Advanced/05DOM-Introduction-Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
-
+const tdRef = document.querySelectorAll('td');
+let counter = 1;
+let sum = 0;
+for (const td of tdRef) {
+    if (counter % 2 == 0) {
+        
+        sum += Number(td.textContent);
+    }
+    counter++;
+}
+document.querySelector('#sum').textContent = sum;
 }
